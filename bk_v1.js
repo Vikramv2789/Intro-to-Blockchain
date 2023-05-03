ac9f9595a92526a6fa13bd8fbc9ced2549bdcd9f
Vikramv2789 <99269958+Vikramv2789@users.noreply.github.com>
2023-05-03 13:51:36 -0400

    Update bk_v1.js

--- bk_v1.js
+++ bk_v1.js
@@ -6,7 +6,7 @@ class Block{
         this.timestamp=timestamp;
         this.data=data;
         this.previousHash=previousHash;
-        this.hash = calculateHash();
+        this.hash = this.calculateHash();
     }
 
 
@@ -19,11 +19,25 @@ calculateHash()
 
 class Blockchain{
     constructor(){
-        this.chain=[]
+        this.chain=[this.createGenesisBlock()]
     }
     createGenesisBlock(){
         return new Block(0, "03/01/2009", "Genesis Block", "0");
     }
+  getLatestBlock(){
+    return this.chain[this.chain.length-1];
+  }
+  addBlock(newBlock){
+    newBlock.previousHash=this.getLatestBlock().hash;
+    newBlock.hash=newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
 }
+
+let btCoin = new Blockchain();
+
+btCoin.addBlock(new Block(1, '1/2/2022',{name: 'TM', amount:4}));
+btCoin.addBlock(new Block(2, '2/2/2022',{name: 'TMI', amount:4}));
+console.log(JSON.stringify(btCoin,null,4));
 b = new Blockchain()
 console.log(b.createGenesisBlock())
